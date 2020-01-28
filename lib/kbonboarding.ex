77c0f0f9090113e464355188c9c488078cb32099
Jonathan Hervault <jonathan.hervault@michelin.com>
2020-01-28 11:34:24 +0100

    init

---
+++ lib/kbonboarding.ex
@@ -0,0 +1,18 @@
+defmodule Kbonboarding do
+  @moduledoc """
+  Documentation for Kbonboarding.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Kbonboarding.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/kbonboarding_test.exs
@@ -0,0 +1,8 @@
+defmodule KbonboardingTest do
+  use ExUnit.Case
+  doctest Kbonboarding
+
+  test "greets the world" do
+    assert Kbonboarding.hello() == :world
+  end
+end
